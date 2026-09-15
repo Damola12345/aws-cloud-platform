e940868d8f13072ac1e7742513f52b781b8201af
Damola12345 <ajiboladamola789@gmail.com>
2026-09-15 11:24:25 +0100

    changed domain name

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -36,13 +36,13 @@ variable "private_subnet_cidrs" {
 variable "domain_name" {
   description = "Root domain already hosted as a public zone in Route53, e.g. dxxxxx.com - also used to look up an already-issued ACM certificate for the ALB (must be ISSUED and cover this domain or a wildcard over it, in the same region as aws_region)"
   type        = string
-  default     = "dglidestcl.com"
+  default     = "xxxx.com"
 }
 
 variable "app_hostname" {
   description = "Full hostname to create and point at the ALB, e.g. finzla-dev.dxxxxx.com. Must fall under domain_name (or its wildcard)."
   type        = string
-  default     = "finzla-dev.dglidestcl.com"
+  default     = "finzla-dev.dxxxxx.com"
 }
 
 variable "create_dns_record" {
